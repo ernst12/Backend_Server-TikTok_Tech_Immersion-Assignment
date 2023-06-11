bd015c5941e75800b67ff99927142c3391ada8ca
ernst12 <50575073+ernst12@users.noreply.github.com>
2023-06-11 16:11:33 +0800

    comment out unused code

--- rpc-server/database/database.model.go
+++ rpc-server/database/database.model.go
@@ -1,13 +1,13 @@
 package database
 
-import ("github.com/ernst12/Backend_Server-TikTok_Tech_Immersion-Assignment/rpc-server/kitex_gen/rpc")
+//import ("github.com/ernst12/Backend_Server-TikTok_Tech_Immersion-Assignment/rpc-server/kitex_gen/rpc")
 
 // will store the chats array in ascending order of the sendTime
-type Database interface {
+/*type Database interface {
 	Append(key string, value *rpc.Message) (error)
 	Get(key string) ([]*rpc.Message, error)
 	Delete(key string) (error)
-}
+}*/
 
 type Message struct {
     Sender    string  `json:"sender"`
